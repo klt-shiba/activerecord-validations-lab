464f37154b86cc95a6e4308ec6a5df69c6e24244
Colin Tseng <klt.aus@yahoo.com>
2021-09-05 12:17:19 +1000

    Almost there

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    
+    validate :title_exists, :non_clickbait
+
+    def non_clickbait
+        if self.title.include? "Won't Believe" || "Secret" || "Top [number]" || "Guess"
+            
+        else
+            errors.add(:title, "can't be non-clickbaity")
+        end
+    end
 end
